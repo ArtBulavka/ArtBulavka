d050b5526a666ef8ac25ab6a5b1369d49f456578
ArtBulavka <91161597+ArtBulavka@users.noreply.github.com>
2021-10-20 17:00:15 +0300

    Add files via upload

--- picture.py
+++ picture.py
@@ -11,21 +11,32 @@ from aiogram.types import ReplyKeyboardRemove, \
 API_TOKEN = '2007008934:AAGvBj5PK78pX5OCDCnIwrjAkYSqB6-bLMI'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-button_hi = KeyboardButton('Привет! 👋')
-keyboard = types.InlineKeyboardMarkup(row_width=1)
-greet_kb = ReplyKeyboardMarkup()
-greet_kb.add(button_hi)
-studyboi = InlineKeyboardButton('text')
-start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(studyboi)
+
+
+
 
 
 @dp.message_handler(content_types="text")
 async def process_hello(message: types.Message):
+    
+    buttons = [
+        types.InlineKeyboardButton(text='hi',callback_data="n"),
+        types.InlineKeyboardButton(text='hi2',callback_data="m")
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
     photo = InputFile("C:/Users/a.bulavchenkov/Desktop/Pict/CAMEL02.jpg")
     await bot.send_photo(message.from_user.id,photo,caption="Привет!Как картинка?", reply_markup=keyboard)
 
 
+@dp.callback_query_handler(text_contains = "n")
+async def callback_data(message: types.CallbackQuery):
+        await bot.send_message(message.from_user.id, "❤️")
 
+
+@dp.callback_query_handler(text_contains = "m")
+async def callback_data(message: types.CallbackQuery):
+        await bot.send_message(message.from_user.id, "🖤")
 
 if __name__== '__main__':
     executor.start_polling(dp, skip_updates=True)
